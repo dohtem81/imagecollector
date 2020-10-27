44d3794246be3b942cdc1251beb5a31870c50aba
Piotr Pedziwiatr <pedziwitr.piotr@gmail.com>
2020-10-27 05:16:52 +0000

    ready to run

--- Program.cs
+++ Program.cs
@@ -9,14 +9,6 @@ namespace cameraimagecollection
         static void Main(string[] args)
         {
             Dictionary<string, utils.Camera> cameraThreads = new Dictionary<string, utils.Camera>();
-            cameraThreads.Add("Camera1", new utils.Camera("Camera1", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/101", 500));
-            cameraThreads.Add("Camera2", new utils.Camera("Camera2", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/201", 500));
-            cameraThreads.Add("Camera3", new utils.Camera("Camera3", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/301", 500));
-            cameraThreads.Add("Camera4", new utils.Camera("Camera4", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/401", 500));
-            cameraThreads.Add("Camera5", new utils.Camera("Camera5", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/501", 500));
-            cameraThreads.Add("Camera6", new utils.Camera("Camera6", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/601", 500));
-            cameraThreads.Add("Camera7", new utils.Camera("Camera7", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/701", 500));
-            cameraThreads.Add("Camera8", new utils.Camera("Camera8", "rtsp://admin:ARadecka@192.168.1.250:554/Streaming/channels/801", 500));
 
             foreach(KeyValuePair<string, utils.Camera> kvp in cameraThreads)
             {
--- utils/camera.cs
+++ utils/camera.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.IO;
 using Emgu.CV;
 
 namespace cameraimagecollection.utils
@@ -14,6 +15,8 @@ namespace cameraimagecollection.utils
         private DateTime creationTime;
         private Mat currentFrame;
         private VideoCapture capture;
+        private bool threadRunning;
+        private readonly object __frameLock = new object();
 
         public Camera(string cameraName, string connectionLink, int phase)
         {
@@ -23,19 +26,62 @@ namespace cameraimagecollection.utils
             this.creationTime = DateTime.Now;
             this.currentFrame = new Mat();
             this.capture = new VideoCapture(this.link);
+            this.threadRunning = true;
 
             this.thread = new Thread(this.MainThread);
             this.thread.IsBackground = true;
             this.thread.Start();
+            (new Thread(this.FrameReadThread)).Start();
         }
 
+        private void FrameReadThread()
+        {
+            while(this.threadRunning)
+            {
+                Mat newFrame = this.capture.QueryFrame();
+                lock(this.__frameLock)
+                {
+                    try
+                    {
+                        this.currentFrame = newFrame.Clone();
+                        //Console.WriteLine("{0} new frame!", this.name);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
         private void MainThread()
         {
-            while((DateTime.Now - this.creationTime).TotalSeconds < 5)
+            while((DateTime.Now - this.creationTime).TotalSeconds < 15)
             {
                 Console.WriteLine("{0} is alive for {1}", this.name, (DateTime.Now - this.creationTime).TotalMilliseconds);
+                lock(this.__frameLock)
+                {
+                    try
+                    {
+                        string filename = string.Format("{0}_{1}.jpg", this.name, DateTime.Now.ToString("HHmmssfff"));
+                        string[] path = {@"/", "var", "collector", "data", filename};
+                        if (!this.currentFrame.IsEmpty)
+                        {
+                            this.currentFrame.Save(Path.Combine(path));
+                            Console.WriteLine("{0} frame saved to {1}", this.name, Path.Combine(path));
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} has empty frame, can't save to {1}", this.name, Path.Combine(path));
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
                 Thread.Sleep(this.phase);
             }
+            this.threadRunning = false;
         }
     }
 }
